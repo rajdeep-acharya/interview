47f574f41d0945771f8da1d4d652e46a7704617c
acra <rajdeep.acharya@skidata.com>
2022-11-24 22:56:17 +0530

    added Talentica assessments

---
+++ Talentica-JavaCode-ShopBabyProducts/src/main/java/com/demo/shop/dto/PriceDTO.java
@@ -0,0 +1,14 @@
+package com.demo.shop.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PriceDTO {
+
+    private Integer shopId;
+    private Float price;
+    private String product_1_label;
+    private String product_2_label;
+    private String product_3_label;
+
+}
